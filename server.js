d422ebbaac6635b740796fe1ad2a1c37e61585f4
Taylor Short <treddshort@gmail.com>
2019-03-13 22:23:23 -0700

    add bootstrap and graphql queries

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const app = express();
+const cors = require('cors');
 const schema = require('./schema.js');
+const app = express();
+
+//Allow cross origin
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
